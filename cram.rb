7c78fbd4b7d1fc97617df18f97b3cccde1e23c88
Roman Neuhauser <roman@sigpipe.cz>
2017-08-21 11:13:02 +0200

    cram-0.7

---
+++ cram.rb
@@ -0,0 +1,14 @@
+class Cram < Formula
+  include Language::Python::Virtualenv
+
+  desc "Functional testing framework for commandline applications"
+  homepage "https://bitheap.org/cram/"
+  url "https://bitheap.org/cram/cram-0.7.tar.gz"
+  sha256 "7da7445af2ce15b90aad5ec4792f857ceft5786d71f14377e9eb994d8b8337f2f"
+
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    virtualenv_install_with_resources
+  end
+end

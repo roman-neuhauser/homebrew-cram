af92226433cca55ff8b8a1f553cec790cfac3a5d
Roman Neuhauser <roman@sigpipe.cz>
2017-09-05 12:02:16 +0200

    fix typo in sha256 value

--- cram.rb
+++ cram.rb
@@ -4,7 +4,7 @@ class Cram < Formula
   desc "Functional testing framework for commandline applications"
   homepage "https://bitheap.org/cram/"
   url "https://bitheap.org/cram/cram-0.7.tar.gz"
-  sha256 "7da7445af2ce15b90aad5ec4792f857ceft5786d71f14377e9eb994d8b8337f2f"
+  sha256 "7da7445af2ce15b90aad5ec4792f857cef5786d71f14377e9eb994d8b8337f2f"
 
   depends_on :python if MacOS.version <= :snow_leopard
 
